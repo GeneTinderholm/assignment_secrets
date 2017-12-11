ba605e4a0b7aa4564daa2ed44203f945cdfbb73f
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-11 12:42:24 -0600

    tomatoes

--- models/secret.js
+++ models/secret.js
@@ -1,17 +1,22 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var SecretSchema = new Schema({
-  body: String,
-  usersRequesting: [{
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  }],
-  userSubmitted: Schema.Types.ObjectId
-}, {
-  timestamps: true
-});
+var SecretSchema = new Schema(
+  {
+    body: String,
+    usersRequesting: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    userSubmitted: Schema.Types.ObjectId
+  },
+  {
+    timestamps: true
+  }
+);
 
-var User = mongoose.model('Secret', SecretSchema);
+var Secret = mongoose.model("Secret", SecretSchema);
 
 module.exports = Secret;
