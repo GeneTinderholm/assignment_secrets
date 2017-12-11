b26de10fdff28b3fb3b3e2653fc422e47c52f47b
Johann <baptistajohann@gmail.com>
2017-12-11 10:59:19 -0800

    stuff

---
+++ services/session.js
@@ -0,0 +1,28 @@
+// /services/Session.js
+const SECRET = process.env["secret"] || "puppies123";
+const md5 = require("md5");
+
+const createSignedSessionId = email => {
+  return `${email}:${generateSignature(email)}`;
+};
+
+const generateSignature = email => md5(email + SECRET);
+
+const User = require("../models/User");
+
+const loginMiddleware = (req, res, next) => {
+  const sessionId = req.cookies.sessionId;
+  if (!sessionId) return next();
+
+  const [email, signature] = sessionId.split(":");
+
+  User.findOne({ email }, (err, user) => {
+    if (signature === generateSignature(email)) {
+      req.user = user;
+      res.locals.currentUser = user;
+      next();
+    } else {
+      res.send("You've tampered with your session!");
+    }
+  });
+};
