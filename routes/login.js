d9b23faa68d2ba11f52b2ee554d07db3a0578fd8
Johann <baptistajohann@gmail.com>
2017-12-11 13:46:42 -0800

    working on login

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var router = express.Router();
-
-/* GET users listing. */
+const models = require('../models/user.js');
+const User = models.User;
+const bcrypt = require('bcrypt');
 
 router.get("/", (req, res) => {
   res.render("login");
+});
+router.post("/", async (req, res) => {
+   let username = req.body.username;
+   let passHash = bcrypt.hashSync(req.body.password, 8);
+   let user = await User.find({
+     where: {
+       username: username,
+       password: req.body.username
 });
 
 module.exports = router;
