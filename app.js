deacb375a3dff5d14b55e7d1913cb691bcb5d5ea
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-11 16:16:31 -0600

    break

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,22 @@
 var express = require("express");
 var router = express.Router();
-const models = require('../models/user.js');
+const models = require("../models/");
 const User = models.User;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   res.render("login");
 });
 router.post("/", async (req, res) => {
-   let username = req.body.username;
-   let passHash = bcrypt.hashSync(req.body.password, 8);
-   let user = await User.find({
-     where: {
-       username: username,
-       password: req.body.username
+  let username = req.body.username;
+  let user = await User.find({
+    username: username
+  });
+  user = user[0];
+  res.session = req.session || {};
+  res.session.loggedIn = user.validatePassword(req.body.password);
+  res.session.userId = user.id;
+  res.redirect("/");
 });
 
 module.exports = router;
