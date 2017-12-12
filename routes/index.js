5ab3efec9114d7adf69698b24fecf7efbbee683f
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-11 18:01:09 -0600

    time ran out

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 var express = require("express");
 var router = express.Router();
-const models = require('../models/');
+const models = require("../models/");
 const User = models.User;
 const Secret = models.Secret;
 
 router.get("/", async (req, res, next) => {
   let user = await User.findById(req.session.userId);
-  let secretsArray = [];
-  user = await User.findById(req.session.userId, {})
+  let secretArray = [];
+  user = await User.findById(req.session.userId, {});
   console.log(user);
-  for(let i = 0; i < user.secrets.length; i++){
-    secretsArray[i] = await Secret.find({
+  for (let i = 0; i < user.secrets.length; i++) {
+    secretArray[i] = await Secret.find({
       _id: user.secrets[i]
-    })
+    });
+    secretArray[i] = secretArray[i][0];
   }
-  console.log(secretsArray);
-  res.render("index");
+  console.log(secretArray);
+  res.render("index", { secretArray });
 });
 module.exports = router;
